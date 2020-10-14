78c7220ca94b18bfc1e9829284ab0e55d1ed705f
SouvikTheRock <60473157+SouvikTheRock@users.noreply.github.com>
2020-10-14 15:05:16 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -3,17 +3,18 @@ include("connect.php");
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $username=$_POST['uname'];
     $password=$_POST['pswd'];
-    $sql="SELECT Email,Password FROM user_details";
+    $sql="SELECT * FROM user_details where Email='$username'";
     $res=mysqli_query($con,$sql)
     or
     die("Query Execution Error");
     echo "Data received";
     while ($row = mysqli_fetch_array($res)){
-        if($row['Email']==$username && $row['Password']==$password){
-            echo '<script>alert("Logged In");window.location.href="dashboard.html";</script>';
+        if($row['Password']!=$password){
+                      echo '<script> alert("Incorrect Password");window.location.href="signin.html";</script>';
         }
         else{
-            echo '<script> alert("No");window.location.href="signin.html";</script>';
+
+              echo '<script>alert("Logged In");window.location.href="dashboard.html";</script>';
         }   
 }  
 }
